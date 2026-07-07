1a1ff4ac6d499c046ecb4bb679b2822223009638
Rajukumar6304 <rajukumar6304.com>
2026-07-07 12:05:37 +0530

    Add BrowserRouter basename

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ import { Redirect } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <HashRouter>
+      {/* <HashRouter>
         <ResponsiveAppBar />
 
         <Switch>
@@ -43,7 +43,7 @@ function App() {
           </Route>
           <Redirect from="/" to="/Users" />
         </Switch>
-      </HashRouter>
+      </HashRouter> */}
     </div>
   );
 }
